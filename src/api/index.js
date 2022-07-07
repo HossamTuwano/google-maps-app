2616c2a7e6a186a1b27553d2707ba278a5b5f8b4
Hossam Tuwano <hossamtuwano@gmail.com>
2022-07-07 23:46:13 +0300

    constrution :chore hid api keys

--- src/api/index.js
+++ src/api/index.js
@@ -1,24 +1,25 @@
 import axios from "axios";
 
-const URL = "https://travel-advisor.p.rapidapi.com/hotels/list-in-boundary";
-
 export const getPlacesData = async (sw, ne) => {
   try {
     const {
       data: { data },
-    } = await axios.get(URL, {
-      params: {
-        bl_latitude: sw.lat,
-        bl_longitude: sw.lng,
-        tr_longitude: ne.lng,
-        tr_latitude: ne.lat,
-      },
+    } = await axios.get(
+      "https://travel-advisor.p.rapidapi.com/hotels/list-in-boundary",
+      {
+        params: {
+          bl_latitude: sw.lat,
+          bl_longitude: sw.lng,
+          tr_longitude: ne.lng,
+          tr_latitude: ne.lat,
+        },
 
-      headers: {
-        "X-RapidAPI-Key": "ebc9a3c867msh1a852796da12e43p1a9767jsn4177cd851cba",
-        "X-RapidAPI-Host": "travel-advisor.p.rapidapi.com",
-      },
-    });
+        headers: {
+          "X-RapidAPI-Key": process.env.REACT_APP_RAPIDAPI_KEY,
+          "X-RapidAPI-Host": "travel-advisor.p.rapidapi.com",
+        },
+      }
+    );
     return data;
   } catch (error) {
     console.log(error);
